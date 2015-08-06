1eb03133a4e7e4f6a8abd5e1c59984db73a3d0dc
emmajane <emma@emmajane.net>
2015-08-06 13:31:33 +0100

    convert.sh: Adjust folder names to accommodate non-book repo.

--- book-svg/convert.sh
+++ book-svg/convert.sh
@@ -5,11 +5,11 @@ for f in $FILES
 do
   echo "Processing $f ..."
   # take action on each file. $f store current file name
-  /Applications/Inkscape.app/Contents/Resources/bin/inkscape -D --export-width=1024 -y 1.0 --export-png ../diagrams/$f.png $f
+  /Applications/Inkscape.app/Contents/Resources/bin/inkscape -D --export-width=1024 -y 1.0 --export-png rendered/$f.png $f
   echo "Renaming file $f.png ..."
-  mv "../diagrams/$f.png" "../diagrams/gft_${f/.svg/}.png"
+  mv "rendered/$f.png" "rendered-png/gft_${f/.svg/}.png"
   
-  /Applications/Inkscape.app/Contents/Resources/bin/inkscape -y 1.0 --export-pdf ../diagrams-pdf/$f.pdf $f
+  /Applications/Inkscape.app/Contents/Resources/bin/inkscape -y 1.0 --export-pdf rendered/$f.pdf $f
   echo "Renaming file $f.pdf ..."
-  mv "../diagrams-pdf/$f.pdf" "../diagrams-pdf/gft_${f/.svg/}.pdf"
+  mv "rendered/$f.pdf" "rendered-pdf/gft_${f/.svg/}.pdf"
 done
